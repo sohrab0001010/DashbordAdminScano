6e45c25229d5a2a325291f1823ffeb126aebb7a6
sohrab0001010 <sohrabg1024@gmail.com>
2026-09-17 23:08:29 +0330

    update

---
+++ src/components/Subscription/Subscription.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Subscription = () => {
+  return (
+    <button
+    className='
+    bg-yellow-300    
+    text-center
+    text-yellow-100
+    px-6
+    py-3
+    rounded-lg
+    cursor-pointer
+    border
+    border-yellow-100
+
+    '
+    >
+        خرید اشتراک
+    </button>
+  )
+}
+
+export default Subscription
--- src/pages/Course.jsx
+++ src/pages/Course.jsx
@@ -3,42 +3,53 @@ import { useParams } from 'react-router';
 import courses from '../data/dataCourses';
 import SectionRight from '../components/Course/SectionRight';
 import SectionLeft from '../components/Course/SectionLeft';
+import Subscription from '../components/Subscription/Subscription';
 
 const Course = () => {
 
-  const course = courses.find(course => 
+  const course = courses.find(course =>
     course.gradeId == useParams().yearId
   )
 
-  const [selectedSection,setSelectedSection] 
-  = useState(course.contentCourse[0]?.name)
+  const [selectedSection, setSelectedSection]
+    = useState(course.contentCourse[0]?.name)
 
 
 
   return (
 
     <div
-    className='
+      className='
     min-h-screen
     bg-white
     '
     >
-    <div className="
-    sectionTop
+      
+      <div className="
+      sectionTop
+      flex
+      items-center
+      justify-between
+      px-18
+      ">
+        <Subscription/>
+
+
+
+        <div className="
     flex
     items-center
     justify-end
     gap-6
-    px-12
     py-5
     ">
-      <span 
-      className="text-2xl font-medium ">
-        {course.nameGrade}-{course.subject}
-      </span>
-      <span
-      style={{ "--course-color": course.color }}
-      className="
+          <span
+            className="text-2xl font-medium ">
+            {course.nameGrade}-{course.subject}
+          </span>
+          <span
+            style={{ "--course-color": course.color }}
+            className="
       bg-[linear-gradient(135deg,color-mix(in_srgb,var(--course-color)_85%,black)_0%,var(--course-color)_28%,color-mix(in_srgb,var(--course-color)_55%,white)_72%,color-mix(in_srgb,var(--course-color)_85%,black)_100%)]
       shadow-[0_10px_35px_-5px_var(--course-color),0_0_20px_-2px_var(--course-color)]
       border-(--course-color)
@@ -48,13 +59,15 @@ const Course = () => {
       text-5xl
       p-4
       "
-      >
-        {course.emoji}
-      </span>
-    </div>
+          >
+            {course.emoji}
+          </span>
+        </div>
 
-    <div
-    className="
+      </div>
+
+      <div
+        className="
     sectionBottom
     select-none
     mainContainer
@@ -65,33 +78,33 @@ const Course = () => {
     px-12
     py-20
     "
-    >
-      <div className="
+      >
+        <div className="
       sectionLeft 
       sticky
       top-8
       self-start
       mt-12
       ">
-        <SectionLeft
-        contentCourse={course.contentCourse}
-        nameGrade={course.nameGrade}
-        onSelect={setSelectedSection}
-        selected={selectedSection}
-        />
-      </div>
+          <SectionLeft
+            contentCourse={course.contentCourse}
+            nameGrade={course.nameGrade}
+            onSelect={setSelectedSection}
+            selected={selectedSection}
+          />
+        </div>
 
-      <div 
-      className="
+        <div
+          className="
       sectionRight
       ">
-        <SectionRight
-        course={course}
-        selectedSection={selectedSection}
-        />
-      </div>
+          <SectionRight
+            course={course}
+            selectedSection={selectedSection}
+          />
+        </div>
 
-    </div>
+      </div>
 
     </div>
 

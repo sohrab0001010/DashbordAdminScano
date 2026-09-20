2c760325126b92b643b56b4bb48d74ae9584a959
sohrab0001010 <sohrabg1024@gmail.com>
2026-09-20 19:06:40 +0330

    update

---
+++ src/data/dataBooks.js
@@ -0,0 +1,50 @@
+const BOOKS = [
+    {
+        id: 1,
+        title: "mathFourth",
+        imgBook: "/images/imgBook/5.jpg",
+        author: "Ahmad Zamani",
+        count: 100,
+        price: 8_000_00
+    },
+    {
+        id: 2,
+        title: "mathFifth",
+        imgBook: "/images/imgBook/6.jpg",
+        author: "Ahmad Zamani",
+        count: 100,
+        price: 8_000_00
+    },
+    {
+        id: 3,
+        title: "mathSixth",
+        imgBook: "/images/imgBook/7.jpg",
+        author: "Ahmad Zamani",
+        count: 100,
+        price: 8_000_00
+    },
+    {
+        id: 4,
+        title: "mathSeventh",
+        imgBook: "/images/imgBook/9.jpg",
+        author: "Ahmad Zamani",
+        count: 100,
+        price: 8_000_00
+    },
+    {
+        id: 5,
+        title: "mathEighth",
+        imgBook: "/images/imgBook/10.jpg",
+        author: "Ahmad Zamani",
+        count: 100,
+        price: 8_000_00
+    },
+    {
+        id: 6,
+        title: "mathNinth",
+        imgBook: "/images/imgBook/11.jpg",
+        author: "Ahmad Zamani",
+        count: 100,
+        price: 8_000_00
+    },
+]

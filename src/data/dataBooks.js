d96bc858e2563af8dbf220cd8262ff0b85d04c84
sohrab0001010 <sohrabg1024@gmail.com>
2026-09-20 23:48:40 +0330

    update

--- src/data/dataBooks.js
+++ src/data/dataBooks.js
@@ -2,49 +2,58 @@ const BOOKS = [
     {
         id: 1,
         title: "mathFourth",
-        imgBook: "/images/imgBook/5.jpg",
-        author: "Ahmad Zamani",
+        name: "ریاضی چهارم",
+        imgBook: "/images/imgBooks/5.jpg",
+        author: "احمدرضا زمانی",
         count: 100,
         price: 8_000_00
     },
     {
         id: 2,
         title: "mathFifth",
-        imgBook: "/images/imgBook/6.jpg",
-        author: "Ahmad Zamani",
+        name: "ریاضی پنجم",
+        imgBook: "/images/imgBooks/6.jpg",
+        author: "احمدرضا زمانی",
         count: 100,
         price: 8_000_00
     },
     {
         id: 3,
         title: "mathSixth",
-        imgBook: "/images/imgBook/7.jpg",
-        author: "Ahmad Zamani",
+        name: "ریاضی ششم",
+        imgBook: "/images/imgBooks/7.jpg",
+        author: "احمدرضا زمانی",
         count: 100,
         price: 8_000_00
     },
     {
         id: 4,
         title: "mathSeventh",
-        imgBook: "/images/imgBook/9.jpg",
-        author: "Ahmad Zamani",
+        name: "ریاضی هفتم",
+        imgBook: "/images/imgBooks/9.jpg",
+        author: "احمدرضا زمانی",
         count: 100,
         price: 8_000_00
     },
     {
         id: 5,
         title: "mathEighth",
-        imgBook: "/images/imgBook/10.jpg",
-        author: "Ahmad Zamani",
+        name: "ریاضی هشتم",
+        imgBook: "/images/imgBooks/10.jpg",
+        author: "احمدرضا زمانی",
         count: 100,
         price: 8_000_00
     },
     {
         id: 6,
         title: "mathNinth",
-        imgBook: "/images/imgBook/11.jpg",
-        author: "Ahmad Zamani",
+        name: "ریاضی نهم",
+        imgBook: "/images/imgBooks/11.jpg",
+        author: "احمدرضا زمانی",
         count: 100,
         price: 8_000_00
     },
 ]
+
+
+export default BOOKS
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Course from "../components/Home/Course";
 import { Link } from "react-router";
 import courses from "../data/dataCourses";
+import books  from "../data/dataBooks";
 
 const Home = () => {
     const course = [
@@ -23,6 +24,7 @@ const Home = () => {
         items-center
         px-6
         py-12
+        gap-12
       "
         >
             <h1
@@ -57,6 +59,47 @@ const Home = () => {
                         />
                     </Link>
                 ))}
+            </div>
+
+            <h2
+            className="
+            text-4xl
+            font-bold
+            text-[#082567]
+            mt-40
+            "
+            >
+                کتاب های گروه آموزشی ویژا
+            </h2>
+
+            <div
+            className="
+            grid
+            grid-cols-3
+            gap-6
+            "
+            >
+                {
+                    books.map((item,index) => (
+                        <Link
+                        key={index}
+                        className="
+                        "
+                        >
+                            <img 
+                            className="
+                            w-12
+                            h-12
+                            "
+                            src={item.imgBook} 
+                            alt={item.name} />
+
+                            <span>نویسنده : {item.author}</span>
+                            <span>قیمت : {item.price} تومان</span>
+                            <span>موجودی : {item.count} عدد</span>
+                        </Link>
+                    ))
+                }
             </div>
         </main>
     );
